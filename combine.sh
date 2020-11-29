f455c08b85be99ca2e1b0871242f2503c942f553
Adam Tran <aviettran@gmail.com>
2020-11-29 14:30:22 -0500

    add build folder

--- combine.sh
+++ combine.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-rm combined.txt -f
+FOLDER=build
+
+rm $FOLDER/combined.txt -f
 
 for f in gen8ou/*
 do
-  echo "=== [gen8ou] $(basename $f .txt) ===" >> combined.txt
-  cat $f >> combined.txt
-  echo "" >> combined.txt
+  echo "=== [gen8ou] $(basename $f .txt) ===" >> $FOLDER/combined.txt
+  cat $f >> $FOLDER/combined.txt
+  echo "" >> $FOLDER/combined.txt
 done
